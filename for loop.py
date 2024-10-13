c37a0d8a8c24291335f3a6a1a20a881a41cf464f
Tamanna-Dahiya <tamannadahiya726@gmail.com>
2024-10-13 13:33:17 +0530

    For loop
    Covering the basic syntax of for loop

---
+++ for loop.py
@@ -0,0 +1,12 @@
+# using for loop to print content of list
+l=[1,2,3,4,5,6,7,8]
+for i in l:
+    print(i)
+# use of for loop with range
+for i in range (5):
+    print(i)
+# using else with for loop
+for i in range (4):
+    print(i)
+else:
+    print("done")       
